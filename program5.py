31699d17dfce2698484e682e88bea6775dbd9dc2
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-07 15:44:17 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,9 @@
+#division
+a=int(input('enter your number :'))
+b=int(input('enter second number :'))
+print("addition :=" , a+b)
+print(" multiply:=" ,a*b)
+print("subtraction :=" ,a-b)
+print("divison:= ", a/b)
+
+
