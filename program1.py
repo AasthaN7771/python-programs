92eaba2e3d2473359ac7e5e12a235b7546f2ae6e
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-07 15:22:46 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,18 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=12
+>>> b=2
+>>> c=a+b
+>>> print(c)
+14
+>>> type(a)
+<class 'int'>
+>>> 
+========================= RESTART: C:/Users/lab/Desktop/aasthaict.py =========================
+12 2 14
+>>> 
+========================= RESTART: C:/Users/lab/Desktop/aasthaict.py =========================
+12 2 14
+>>> 
+========================= RESTART: C:/Users/lab/Desktop/aasthaict.py =========================
+14
