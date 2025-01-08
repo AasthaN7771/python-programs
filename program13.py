c8ee1b5c8fd093b0be2a305860376dcf8a0fb2a9
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-08 19:40:51 +0530

    Add files via upload

---
+++ program13.py
@@ -0,0 +1,12 @@
+# convert bytes in kb
+a = int(input(" enter the size in kb:"))
+b = a*1024
+print(" size in bytes is : " ,b)
+# convert bytes in mb
+c = int(input(" enter the size in mb:"))
+d = c*1048576 
+print(" size in bytes is : " ,d) 
+# convert bytes in gb
+e = int(input(" enter the size in gb:"))
+f = e*1073741824 
+print(" size in bytes  is : " ,f) 
