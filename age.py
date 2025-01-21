d5e02e9e54840b4b1ea89f387d91c251ad8c7a0b
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 15:13:27 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,26 @@
+#divisibility by10
+def pro5():
+    a=int(input("enter the age:"))  
+    
+    
+    
+    if a>18 :
+        print( " major")
+       
+    else :
+        print( " minor ")
+            
+
+
+pro5()
+    
+       
+      
+
+
+   
+ 
+     
+     
+
+    
