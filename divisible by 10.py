c6eab29824b86880868ef3ac641b27ce43b2a8e7
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 15:11:07 +0530

    Add files via upload

---
+++ divisible by 10.py
@@ -0,0 +1,26 @@
+#divisibility by10
+def pro4():
+    a=int(input("enter a number:"))  
+    
+    
+    
+    if a%10==0 :
+        print( " number is divisible ")
+       
+    else :
+        print( " number is not divisible")
+            
+
+
+pro4()
+    
+       
+      
+
+
+   
+ 
+     
+     
+
+    
