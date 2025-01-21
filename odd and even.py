7ab6747ec0c91ba6e61f6208324355cf5ccac67c
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 15:05:59 +0530

    Add files via upload

---
+++ odd and even.py
@@ -0,0 +1,25 @@
+#odd and even
+def pro3():
+    
+    
+    if a%2==0 :
+        return "ëven"
+       
+    else :
+        return "odd"
+a=int(input("enter a number:"))     
+result=pro3()
+print(f"the a is {result} ")
+
+pro3()
+    
+       
+      
+
+
+   
+ 
+     
+     
+
+    
