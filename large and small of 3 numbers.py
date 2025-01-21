65be813cfe50d0beb7f1e3013fc2820b35effd5f
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 14:42:43 +0530

    Add files via upload

---
+++ large and small of 3 numbers.py
@@ -0,0 +1,24 @@
+#largest and smallest values
+def pro2():
+ a =int(input("enter a number: "))
+ b=int(input("enter other number: "))
+ c=int(input("enter another number : "))
+ if a > b and a > c:
+    print("a is largest ",a)
+ elif b > a and b >c:
+    print("b is largest ",b)
+ elif c >a and c>b:
+    print("c is largest ",c)
+ if a > b :
+    print(" b is smallest ",b)
+ elif b >a and b > c :
+    print(" c is smallest ",c)
+ elif c >a and c>b:
+    print("a is smallest ",a)
+
+pro2()
+pro2()
+    
+     
+
+    
