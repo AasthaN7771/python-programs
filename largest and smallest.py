c9c232f1647ab5088f605417cd150ed2146afe94
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 14:29:10 +0530

    Add files via upload

---
+++ largest and smallest.py
@@ -0,0 +1,13 @@
+#largest and smallest values
+a=int(input("enter a number "))
+b=int(input("enter other number "))
+if a > b :
+    print("a is largest ",a)
+    print( "b is smallest ",b)
+elif b > a :
+    print("b is largest ",b)
+    print( "a is smallest ",a)
+else:
+     print("both are same values ")
+
+    
