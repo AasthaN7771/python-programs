4a7f3430ea7ae84448d56e8693b722cee4652e77
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 15:39:08 +0530

    Add files via upload

---
+++ valid triangle.py
@@ -0,0 +1,25 @@
+def pro8():
+    a=int(input("enter 1st angle :"))  
+    b=int(input("enter 2nd angle :"))
+    c=int(input("enter 3rdangle :"))
+    d = a+b+c
+    
+    
+    if d>180  :
+        print( " not a valid triangle ")
+       
+    else :
+        print( " is a valid triangle  ")
+            
+
+
+pro8()
+    
+
+
+   
+ 
+     
+     
+
+    
