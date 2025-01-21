ec2b8779c359c94b9c0c293144ccf701a814f430
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 15:34:32 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,23 @@
+def pro7():
+    a=int(input("enter a year :"))  
+    
+    
+    
+    if  a%4==0 or a%100 ==0 or a%400 ==0 :
+        print( " leap year")
+       
+    else :
+        print( "not a leap year ")
+            
+
+
+pro7()
+    
+
+
+   
+ 
+     
+     
+
+    
