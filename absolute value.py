b974c60caa5bbd38a7fa77cc4c982a21f0c8970b
AasthaN7771 <aastha.nanda123@gmail.com>
2025-01-21 15:52:15 +0530

    Add files via upload

---
+++ absolute value.py
@@ -0,0 +1,19 @@
+def pro9(number):
+     if number<0:
+         return -number
+       
+     else:
+         return number
+        
+            
+num = int(input("enter a number :"))
+result=pro9(num)
+print(f"the  absolute value of {num} is {result}")
+    
+
+
+   
+ 
+     
+     
+
