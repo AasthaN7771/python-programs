8f0da3a8f767043f550d759feaed04b2e69742f9
AasthaN7771 <aastha.nanda123@gmail.com>
2025-02-04 15:41:16 +0530

    Add files via upload

---
+++ string3.py
@@ -0,0 +1,12 @@
+
+def check_string(mainstring,substring):
+    if substring in mainstring :
+        print("yes")
+    else :
+        print("no")
+
+mainstring=input("enter a string :")
+substring=input(" enter another string :")
+
+
+check_string(mainstring, substring)
