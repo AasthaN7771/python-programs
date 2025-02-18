891fcef16815034225904df0495b2f6a7b69fdae
AasthaN7771 <aastha.nanda123@gmail.com>
2025-02-18 15:00:18 +0530

    Add files via upload

---
+++ tuple2.py
@@ -0,0 +1,7 @@
+students=[('24bit01','krishna','18'),('24bit02','aastha','17'),('24bit03','shreya','18')]
+roll_no=[student[0] for student in students]
+name=[student[1] for student in students]
+age=[student[2] for student in students]
+print("roll no are ",roll_no)
+print("name are",name)
+print("age are",age)
